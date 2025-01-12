8ae42ca22662daf0ecb1d67568f0cc080044f227
Mainu0177 <mainuddinhassan658@gmail.com>
2025-01-12 15:17:35 +0600

    copy client

---
+++ client copy/src/app/page.tsx
@@ -0,0 +1,11 @@
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="" >
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        hii
+      </main>
+    </div>
+  );
+}
