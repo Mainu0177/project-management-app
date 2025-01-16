894668bc803370f7e2cd88e864119bc601cdbdf3
Mainu0177 <mainuddinhassan658@gmail.com>
2025-01-16 23:59:23 +0600

    updateds

---
+++ frontend/src/app/projects/[id]/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { useState } from "react";
+
+type Props = {
+    params: {id: string}
+}
+
+const Project = ({ params }: Props) =>{
+    const { id } = params; 
+    const [activeTab, setActiveTab] = useState("Board");
+    const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false)
+
+    return <div>
+        {/* MODAL NEW TASKS */}
+        <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+        activeTab
+    </div>
+}
+
+export default Project;
